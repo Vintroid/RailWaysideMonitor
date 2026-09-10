2e0431531bd95156876d7d07e9307c8e12db02d4
Vincent St-Pierre <vstpierre1@gmail.com>
2026-09-09 21:30:17 -0400

    Added: Test case WaysideDevice_InitializesWithExpectedState

--- RailWaysideMonitor.Tests/UnitTest1.cs
+++ RailWaysideMonitor.Tests/UnitTest1.cs
@@ -1,10 +1,20 @@
-﻿namespace RailWaysideMonitor.Tests;
+﻿using RailWaysideMonitor.Enums;
+using RailWaysideMonitor.Types;
+
+namespace RailWaysideMonitor.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void WaysideDevice_InitializesWithExpectedState()
     {
+        WaysideDevice device = new()
+        {
+            Id = "TRACK-A",
+            Type = "TrackCircuit",
+            State = DeviceState.Cleared
+        };
 
+        Assert.Equal(DeviceState.Cleared,device.State);
     }
 }
